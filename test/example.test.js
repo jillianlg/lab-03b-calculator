88ce54c6f739415aa88a83d0875623530dfdd77c
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-09-04 14:52:32 -0700

    add test.js paramiters

--- test/example.test.js
+++ test/example.test.js
@@ -3,14 +3,64 @@
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('sumTwoNumbers should return 10 if passed 5 and 5', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = 10;
+    const sumInput1 = 5;
+    const sumInput2 = 5;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = sumTwoNumbers(sumInput1, sumInput2);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('subTwoNumbers should return 1 if passed 6 and 5', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 1;
+    const subInput1 = 6;
+    const subInput2 = 5;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = subTwoNumbers(subInput1, subInput2);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('mulTwoNumbers should return 25 if passed 5 and 5', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 25;
+    const mulInput1 = 5;
+    const mulInput2 = 5;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = mulTwoNumbers(mulInput1, mulInput2);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('divTwoNumbers should return 6 if passed 30 and 5', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 6;
+    const divInput1 = 30;
+    const divInput2 = 5;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = divTwoNumbers(divInput1, divInput2);
 
     //Expect
     // Make assertions about what is expected versus the actual result
